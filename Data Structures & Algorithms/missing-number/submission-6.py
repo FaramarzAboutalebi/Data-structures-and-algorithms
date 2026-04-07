3cc5dd4916700c962d3ff9cd03317020e667bf03
Faramarz Aboutalebi <111402088+FaramarzAboutalebi@users.noreply.github.com>
2026-04-07 00:35:32 -0400

    Add: missing-number - submission-6

---
+++ Data Structures & Algorithms/missing-number/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+
+        xor = len(nums)
+
+        for i in range(len(nums)):
+            xor ^= (i ^ nums[i])
+        
+        return xor
+
+
+# time complexity: O(n)
+# space complexity: O(1)
+        
