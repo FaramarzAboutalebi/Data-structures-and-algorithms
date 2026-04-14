8001d6e29d0d4abaac072257eed1f889b7742712
Faramarz Aboutalebi <111402088+FaramarzAboutalebi@users.noreply.github.com>
2026-04-14 03:18:40 -0400

    Add: decode-ways - submission-10

---
+++ Data Structures & Algorithms/decode-ways/submission-10.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        if s[0] == '0':
+            return 0
+
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(1,len(s)):
+            if '1' <= s[i] <= '9':
+                dp[i+1] += dp[i]
+            
+            two_digt = s[i-1 : i+1]
+
+            if "10" <= two_digt <= "26":
+                dp[i+1] += dp[i-1]
+            
+        return dp[-1]
+
+# time complexity: O(n)
+# space complexity: O(n)
