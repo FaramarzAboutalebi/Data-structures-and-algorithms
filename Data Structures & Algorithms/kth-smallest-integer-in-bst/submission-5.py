4c20213a58a9d501f972cfcf1f996bc7a54ed8e4
Faramarz Aboutalebi <111402088+FaramarzAboutalebi@users.noreply.github.com>
2026-04-24 01:52:40 -0400

    Add: kth-smallest-integer-in-bst - submission-5

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-5.py
@@ -0,0 +1,28 @@
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left= left
+#         self.right= right
+
+class Solution:
+    def kthSmallest(self, root: Optinal[TreeNode], k:int)->List[int]:
+        
+        stack = []
+        cur = root
+        
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            
+            node = stack.pop()
+
+            k -= 1
+
+            if k == 0:
+                return node.val
+            
+            cur = node.right
+
+# time complexity: O(h+k)
+# space complexity: O(h)
