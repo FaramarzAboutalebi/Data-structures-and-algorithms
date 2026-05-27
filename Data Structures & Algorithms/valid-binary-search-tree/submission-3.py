49700fe60eb371c9c7f806c7b7d22647a0381886
Faramarz Aboutalebi <111402088+FaramarzAboutalebi@users.noreply.github.com>
2026-05-27 18:23:09 -0400

    Add: valid-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-3.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def dfs(cur, leftRange, rightRange):
+            if not cur:
+                return True
+            if not (leftRange < cur.val < rightRange):
+                return False
+            
+            return dfs(cur.left, leftRange, cur.val) and dfs(cur.right, cur.val, rightRange)
+            
+
+        return dfs(root, float("-inf"), float("inf"))
+
+# time complexity: O(n)
+# space complexity: O(h)
