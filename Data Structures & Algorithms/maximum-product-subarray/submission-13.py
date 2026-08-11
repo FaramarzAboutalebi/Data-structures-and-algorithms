a7544eaec00614b52cc0c1f6383515a1316b2e5d
Faramarz Aboutalebi <111402088+FaramarzAboutalebi@users.noreply.github.com>
2026-08-11 01:49:36 -0400

    Add: maximum-product-subarray - submission-13

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-13.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        maxP,minP = 1,1
+        res = float("-inf")
+
+        for n in nums:
+
+            maxP,minP= max(n,maxP * n, minP * n),min(n,maxP * n, minP * n)
+
+            res = max(res, maxP) 
+
+            if n == 0:
+                maxP,minP = 1,1
+
+        return res
+
+# time complexity: O(n)
+# space complexity: O(1)
