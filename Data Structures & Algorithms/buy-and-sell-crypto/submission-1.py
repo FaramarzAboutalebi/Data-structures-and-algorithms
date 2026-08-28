c1c44669cc28309f1ad02b026021977dee593844
Faramarz Aboutalebi <111402088+FaramarzAboutalebi@users.noreply.github.com>
2026-08-27 22:45:57 -0500

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        l = 0
+        res = 0
+
+        for r in range(len(prices)):
+            if prices[l] > prices[r]:
+                l = r
+            res = max(res, prices[r] - prices[l]) 
+        return res 
+
+# time complexity: O(n)
+# space complexity: O(1)
