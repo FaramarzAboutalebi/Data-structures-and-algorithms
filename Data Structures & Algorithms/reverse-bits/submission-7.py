540a68a4e5d30d294ea47cee437bc5a8e9daa05e
Faramarz Aboutalebi <111402088+FaramarzAboutalebi@users.noreply.github.com>
2026-09-10 03:23:13 -0400

    Add: reverse-bits - submission-7

---
+++ Data Structures & Algorithms/reverse-bits/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+
+        res = 0
+
+        for i in range(32):
+
+            res += ((n & 1) << (31 -i))
+            n >>= 1
+        return res
+        
+# time complexity: O(1)
+# space complexity: O(1)
