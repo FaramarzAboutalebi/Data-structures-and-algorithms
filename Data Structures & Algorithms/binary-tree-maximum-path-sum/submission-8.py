2a354afbe9220d9d5f7d22fcc382b12d37a6ec07
Faramarz Aboutalebi <111402088+FaramarzAboutalebi@users.noreply.github.com>
2026-09-20 03:47:12 -0400

    Add: binary-tree-maximum-path-sum - submission-8

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-8.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+class Solution:
+  def maxPathSum(self, root:Optional[TreeNode])->int:
+
+    self.res = float("-inf")
+    
+    def dfs(cur):
+        if not cur:
+            return 0
+    
+        left = dfs(cur.left)
+        right = dfs(cur.right)
+
+        maxLeft,maxRight = max(left,0),max(right,0)
+
+        self.res = max(self.res, maxLeft + maxRight + cur.val)
+
+        return max(maxLeft + cur.val, maxRight + cur.val)
+    dfs(root)
+    return self.res
+
+# time complexity: O(n)
+# space complexity: O(h)
+    # avg: O(log n)
+    # worst case: O(n)
